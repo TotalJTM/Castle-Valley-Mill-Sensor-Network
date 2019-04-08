d2026e5ad98483f6bf94472f668b95232461e550
mattmbm <49252829+mattmbm@users.noreply.github.com>
2019-04-08 17:51:46 -0400

    Update models.py

--- models.py
+++ models.py
@@ -57,28 +57,41 @@ class Device(db.Model):
 		return generate_json
 
 	def new_sensor(passed_id,sensor_id,sensor_title=None,sensor_type):
-		element = Device.query.filter_by(assigned_id=passed_device_id).first()
-		new_sensor = Sensor(assigned_id=form.entry_assigned_id.data,title=form.entry_title.data,sensor_type=form.entry_sensor_type.data)
-		Device.new_sensor(form.entry_device_id, )
+		element = Device.query.filter_by(assigned_id=passed_id).first()
+		new_sensor = Sensor(assigned_id=sensor_id,title=sensor_title,sensor_type=sensor_type)
 		element.sensors.append(new_sensor)
 		db.session.add(obj)
 		db.session.commit()
 
 	def remove_sensor(passed_id,sensor_id):
-		element = Device.query.filter_by(assigned_id=passed_device_id).first()
+		element = Device.query.filter_by(assigned_id=passed_id).first()
 		db.session.delete(element)
 		db.session.commit()
 
 	def get_sensor_data(passed_id,sensor_id,nDatapoints):
-		element = Device.query.filter_by(assigned_id=passed_device_id).first()
-		generate_json = f'{{"assigned_id":{element.assigned_id},"title":{element.title},"sensor_data":{element.sensor_data[:10]}}}'
+		element = Device.query.filter_by(assigned_id=passed_id).first()
+		for j in element.sensors:
+			if j.assigned_id == sensor_id:
+				generate_json = f'{{"assigned_id":{j.assigned_id},"title":{j.title},"sensor_data":{j.sensor_data[:nDatapoints]}}}'		
+
 		return generate_json
 		db.session.commit()
 
 	def add_sensor_data(passed_id,sensor_id,data):
-		element = Device.query.filter_by(assigned_id=passed_device_id).first()
+		data_entry = SensorData.create(data)
+		element = Device.query.filter_by(assigned_id=passed_id).first()
+		for j in element.sensors:
+			if j.assigned_id == sensor_id:
+				j.sensor_data.append(data_entry)
 		db.session.add(obj)
 		db.session.commit()
+
+	def add_battery_data(passed_id,sensor_id,data):
+		data_entry = SensorData.create()
+		element = Device.query.filter_by(assigned_id=passed_id).first()
+		for j in element.battery_data:
+			if j.assigned_id == sensor_id:
+				j.battery_data.append(data_entry)
 
 
 class BatteryData(db.Model):
@@ -87,6 +100,17 @@ class BatteryData(db.Model):
 	data = db.Column(db.Integer, unique=False, nullable=False)
 	timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 	parent_id = db.Column(db.Integer, db.ForeignKey('device.id'), nullable=False)
+
+	def __init__(self,data,parent_id):
+		self.data =data
+		self.parent_id = parent_id
+
+@classmethod
+	def create(cls, **kw):
+		obj = cls(**kw)
+		db.session.add(obj)
+		db.session.commit()
+
 
 class Sensor(db.Model):
 	__bind_key__ = 'network'
@@ -104,7 +128,6 @@ class Sensor(db.Model):
 		self.title = title
 		self.sensor_type = sensor_type
 		
-
 	@classmethod
 	def create(cls, **kw):
 		obj = cls(**kw)
@@ -119,6 +142,15 @@ class SensorData(db.Model):
 	timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 	parent_id = db.Column(db.Integer, db.ForeignKey('sensor.id'), nullable=False)
 
+	def __init__(self,new_data):
+		self.data=new_data
+
+	@classmethod
+	def create(cls, **kw):
+		obj = cls(**kw)
+		db.session.add(obj)
+		db.session.commit()
+
 class SensorEvent(db.Model):
 	__bind_key__ = 'network'
 	id = db.Column(db.Integer, primary_key=True)
@@ -126,4 +158,11 @@ class SensorEvent(db.Model):
 	threshold_val = db.Column(db.String(20), unique=False, nullable=False)
 	threshold_comparator = db.Column(db.String(2), unique=False, nullable=False)
 	parent_id = db.Column(db.Integer, db.ForeignKey('sensor.id'), nullable=False)
+	
 
+
+	@classmethod
+	def create(cls, **kw):
+		obj = cls(**kw)
+		db.session.add(obj)
+		db.session.commit()
