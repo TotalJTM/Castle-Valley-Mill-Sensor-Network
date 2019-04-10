40090030a059811377206f2c9ed567d9444369bc
Jeremy McGinnis <jermcg22@gmail.com>
2019-04-09 23:37:51 -0400

    Necessary changes to fix json generating

--- models.py
+++ models.py
@@ -19,6 +19,11 @@ class User(db.Model, UserMixin):
 	def __repr__(self):
 		return f"User('{self.username}')"
 
+	def get_user_data(username):
+		element = Device.query.filter_by(username=username).first()
+		generate_json = f'{{"user_current":{element.username},"user_permission":{element.perms}}}'
+		return generate_json
+
 class Device(db.Model):
 	__bind_key__ = 'network'
 	id = db.Column(db.Integer, primary_key=True)
@@ -53,14 +58,18 @@ class Device(db.Model):
 
 	def get_data(passed_id):
 		element = Device.query.filter_by(assigned_id=passed_id).first()
+		display_data = []
+		counter = 0
+		for i in reversed(j.battery_data):
+			if counter < nDatapoints:
+				display_data.append(f'{{"data":{i.data},"timestamp":{i.timestamp}}}')
 		generate_json = f'{{"assigned_id":{element.assigned_id},"title":{element.title},"mill_floor":{element.mill_floor},"battery_type":{element.battery_type},"battery_data":{element.battery_data[:10]},"sensors":{element.sensors}}}'
 		return generate_json
 
-	def new_sensor(passed_id,sensor_id,sensor_title=None,sensor_type):
+	def new_sensor(passed_id,sensor_id,sensor_type,sensor_title=None):
 		element = Device.query.filter_by(assigned_id=passed_id).first()
 		new_sensor = Sensor(assigned_id=sensor_id,title=sensor_title,sensor_type=sensor_type)
 		element.sensors.append(new_sensor)
-		db.session.add(obj)
 		db.session.commit()
 
 	def remove_sensor(passed_id,sensor_id):
@@ -72,26 +81,28 @@ class Device(db.Model):
 		element = Device.query.filter_by(assigned_id=passed_id).first()
 		for j in element.sensors:
 			if j.assigned_id == sensor_id:
-				generate_json = f'{{"assigned_id":{j.assigned_id},"title":{j.title},"sensor_data":{j.sensor_data[:nDatapoints]}}}'		
+				display_data = []
+				counter = 0
+				for i in reversed(j.sensor_data):
+					if counter < nDatapoints:
+						display_data.append(f'{{"data":{i.data},"timestamp":{i.timestamp}}}')
+				generate_json = f'{{"assigned_id":{j.assigned_id},"title":{j.title},"sensor_type":{j.sensor_type},"sensor_data":{display_data}}}'		
+				return generate_json
+		return 'err-nosensor'
 
-		return generate_json
-		db.session.commit()
-
-	def add_sensor_data(passed_id,sensor_id,data):
-		data_entry = SensorData.create(data)
+	def new_sensor_data(passed_id,sensor_id,new_data):
+		data_entry = SensorData(data=new_data)
 		element = Device.query.filter_by(assigned_id=passed_id).first()
 		for j in element.sensors:
 			if j.assigned_id == sensor_id:
 				j.sensor_data.append(data_entry)
-		db.session.add(obj)
 		db.session.commit()
 
-	def add_battery_data(passed_id,sensor_id,data):
-		data_entry = SensorData.create()
+	def new_battery_data(passed_id,new_data):
+		data_entry = BatteryData(data=new_data)
 		element = Device.query.filter_by(assigned_id=passed_id).first()
-		for j in element.battery_data:
-			if j.assigned_id == sensor_id:
-				j.battery_data.append(data_entry)
+		element.battery_data.append(data_entry)
+		db.session.commit()
 
 
 class BatteryData(db.Model):
@@ -100,17 +111,6 @@ class BatteryData(db.Model):
 	data = db.Column(db.Integer, unique=False, nullable=False)
 	timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 	parent_id = db.Column(db.Integer, db.ForeignKey('device.id'), nullable=False)
-
-	def __init__(self,data,parent_id):
-		self.data =data
-		self.parent_id = parent_id
-
-@classmethod
-	def create(cls, **kw):
-		obj = cls(**kw)
-		db.session.add(obj)
-		db.session.commit()
-
 
 class Sensor(db.Model):
 	__bind_key__ = 'network'
@@ -123,33 +123,12 @@ class Sensor(db.Model):
 	events = db.relationship('SensorEvent', backref='sensor', lazy=True)
 	parent_id = db.Column(db.Integer, db.ForeignKey('device.id'), nullable=False)
 
-	def __init__(self,assigned_id,title=None,sensor_type):
-		self.assigned_id = assigned_id
-		self.title = title
-		self.sensor_type = sensor_type
-		
-	@classmethod
-	def create(cls, **kw):
-		obj = cls(**kw)
-		db.session.add(obj)
-		db.session.commit()
-
-
 class SensorData(db.Model):
 	__bind_key__ = 'network'
 	id = db.Column(db.Integer, primary_key=True)
 	data = db.Column(db.String(20), unique=False, nullable=False)
 	timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 	parent_id = db.Column(db.Integer, db.ForeignKey('sensor.id'), nullable=False)
-
-	def __init__(self,new_data):
-		self.data=new_data
-
-	@classmethod
-	def create(cls, **kw):
-		obj = cls(**kw)
-		db.session.add(obj)
-		db.session.commit()
 
 class SensorEvent(db.Model):
 	__bind_key__ = 'network'
@@ -158,11 +137,3 @@ class SensorEvent(db.Model):
 	threshold_val = db.Column(db.String(20), unique=False, nullable=False)
 	threshold_comparator = db.Column(db.String(2), unique=False, nullable=False)
 	parent_id = db.Column(db.Integer, db.ForeignKey('sensor.id'), nullable=False)
-	
-
-
-	@classmethod
-	def create(cls, **kw):
-		obj = cls(**kw)
-		db.session.add(obj)
-		db.session.commit()
